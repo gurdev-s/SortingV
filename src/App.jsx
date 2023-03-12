bf6f8b6127dff109d111a3d4dd478254523db9aa
gurdev-s <70900597+gurdev-s@users.noreply.github.com>
2023-03-12 16:03:32 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Visualizer from './SortingVisualizer/Visualizer';
+
+function App() {
+	return (
+		<>
+			<Visualizer />
+		</>
+	);
+}
+
+export default App;
